9ceae77cdbf3a995fa53ae221e7fe4bf54b0da2d
jay <jay@jay-cohen.info>
2021-11-21 17:10:31 -0800

    some localization for sally

--- mail_rulez.py
+++ mail_rulez.py
@@ -1,3 +1,6 @@
+#!/home/sally/mail-rulez/venv/bin/python
+# The above shebang points to a virtualenv on sally
+
 import functions as pf
 from dotenv import load_dotenv
 import os
@@ -11,14 +14,14 @@ password = os.getenv("jaypass")
 scheduler = BlockingScheduler(timezone="US/Pacific")
 
 #  Inbox
-scheduler.add_job(lambda: pf.process_inbox(server,account,password), "interval", minutes=10)
+scheduler.add_job(lambda: pf.process_inbox(server,account,password), "interval", minutes=5)
 #  Approved
-scheduler.add_job(lambda: pf.process_folder("lists/white.txt",server,account,password,"INBOX._Approved", "INBOX.Processed"), "interval", minutes=8)
+scheduler.add_job(lambda: pf.process_folder("/home/sally/mail-rulez/lists/white.txt",server,account,password,"INBOX._Approved", "INBOX.Processed"), "interval", minutes=4)
 #  Junk
-scheduler.add_job(lambda: pf.process_folder("lists/black.txt",server,account,password,"INBOX._Rejected", "INBOX.Junk"), "interval", minutes=8)
+scheduler.add_job(lambda: pf.process_folder("/home/sally/mail-rulez/lists/black.txt",server,account,password,"INBOX._Rejected", "INBOX.Junk"), "interval", minutes=4)
 #  Vendor
-scheduler.add_job(lambda: pf.process_folder("lists/vendor.txt",server,account,password,"INBOX._Vendor", "INBOX.Approved_Ads"), "interval", minutes=8)
+scheduler.add_job(lambda: pf.process_folder("/home/sally/mail-rulez/lists/vendor.txt",server,account,password,"INBOX._Vendor", "INBOX.Approved_Ads"), "interval", minutes=4)
 #  Head
-scheduler.add_job(lambda: pf.process_folder("lists/head.txt",server,account,password,"INBOX._HH", "INBOX.HeadHunt"), "interval", minutes=8)
+scheduler.add_job(lambda: pf.process_folder("/home/sally/mail-rulez/lists/head.txt",server,account,password,"INBOX._HH", "INBOX.HeadHunt"), "interval", minutes=4)
 
 scheduler.start()
